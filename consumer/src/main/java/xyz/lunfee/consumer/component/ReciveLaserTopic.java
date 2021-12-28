269001b14974241b063a9472c6e9e505bb1a4f7b
lunfee <1060970432@qq.com>
2021-12-28 17:38:06 +0800

    同步到linux

--- consumer/src/main/java/xyz/lunfee/consumer/component/ReciveLaserTopic.java
+++ consumer/src/main/java/xyz/lunfee/consumer/component/ReciveLaserTopic.java
@@ -28,17 +28,20 @@ public class ReciveLaserTopic {
 //        log.info("Receive a new message"+laser);
         LaserMessage laserMessage = JSON.parseObject(laser, LaserMessage.class);
         String ymdhms = TimestampUtils.stampToDate((int) laserMessage.getSecs());
+        //由 laserScan的yyyymmddhhMMss --> mmddhhMMss
         String mdhms = ymdhms.substring(4, 14);
+        //laserScan的9位纳秒数据 --> 3位毫秒数据
         String ms = laserMessage.getNsecs().substring(0, 3);
 
 
         double[] ranges = laserMessage.getRanges();
         double[] intensities = laserMessage.getIntensities();
 
+        //唯一Rowkey由"L"代表LaserScan，由mmddhhMMss + SSS代表唯一时间标识，便于字典查找
         String rowKey = "L" + mdhms + ms;
         String range = JSON.toJSONString(ranges);
         String intensity = JSON.toJSONString(intensities);
-//        HbaseClientUtils.addData("laser", rowKey, "ranges", "range", range);
+        HbaseClientUtils.addData("laser", rowKey, "ranges", "range", range);
     }
 
 
