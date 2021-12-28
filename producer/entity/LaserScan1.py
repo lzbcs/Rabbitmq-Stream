af53f3bd89459025a1674208a7ebcc5be8a545ea
lunfee <1060970432@qq.com>
2021-12-28 23:38:34 +0800

    gitignore

--- producer/entity/LaserScan1.py
+++
@@ -1,9 +0,0 @@
-class Laser:
-    def __init__(self, min, max, increase, ranges,):
-        self.min = min
-        self.max = max
-        self.increase = increase
-        self.ranges = ranges
-
-
-
