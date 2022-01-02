dad6138fc3aa7acd173085bfbea6a61d122d6f8a
lunfee <1060970432@qq.com>
2022-01-02 15:55:24 +0800

    publisher

--- producer/LaserMessage.py
+++
@@ -1,6 +0,0 @@
-class LaserMessage(object):
-    def __init__(self, secs, nsecs, intensities, ranges):
-        self.secs = secs
-        self.nsecs = nsecs
-        self.intensities = intensities
-        self.ranges = ranges
